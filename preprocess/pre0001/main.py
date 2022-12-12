6554b0d95d5b50874da12dbf1afc324efd937857
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-12-12 21:07:59 +0900

    sub0000 debug

--- preprocess/pre0001/main.py
+++ preprocess/pre0001/main.py
@@ -1,6 +1,9 @@
 '''
+
     前処理の大きな流れ
-    
+
+    2. dataを読み込む
+
     1.  3つのデータ(water, rain, tide)についてそれぞれ,
             - duplicate
             - 同一station
@@ -10,8 +13,18 @@
         に対し前処理を行う。
     
     2.  (station, city)または(station, river)の組み合わせを
-        keyとしたdatabaseを作る。
+        idとしたdatabaseを作る。
     
-    3.  
+    3.  測定値の入ったデータ(data.csv)を、columnsをstationに
+        して測定値を時系列で一列に変換する。
+
+    4.  河川名と水系名について前処理を施し、河川名、水系名それぞれ
+        idとしたdatabaseを作る。全データの河川名と水系名をidで
+        表す。
 
 '''
+from pathlib import Path
+
+
+def main():
+    pass
