91c88f62ccc596ea39af1a5b6f3958c5ef75cf4a
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-12-12 20:14:23 +0900

    sub0001 debug

--- sub/sub0001/src/predictor.py
+++ sub/sub0001/src/predictor.py
@@ -154,6 +154,9 @@ def postprocess(preds_all, stations):
     df = df.stack().reset_index().rename(columns={0: 'value'})
     print(df)
     print(df.columns)
+    df['hour'] = df['hour'].astype(int)    
+    df['station'] = df['station'].astype(str)    
+    df['value'] = df['value'].astype(float)    
     aaa
     output = df.to_dict('records')
     return output
