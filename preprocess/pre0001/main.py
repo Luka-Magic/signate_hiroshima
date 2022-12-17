8436cb4f6f477478c83280ce8597249f59b70fb5
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-12-17 12:31:52 +0900

    exp0018

--- preprocess/pre0001/main.py
+++ preprocess/pre0001/main.py
@@ -8,17 +8,16 @@ from rain import rain_process1
 from tide import tide_process1
 from timeseries import convert_timeseries
 from river import river_process1
+import warnings
+warnings.simplefilter('ignore')
 
-def main(data_dir, save_dir):
+def main(data_dir):
     '''
 
         data_dir: str or Path
             signateからダウンロードできるデータ(train.zip)を解凍したディレクトリ
             "train"がdata_dirの最後に来るようにする
-        save_dir: str or Path
-            保存先のディレクトリ
-            9つのcsvファイルが出力される
-
+        
         --------------------------------
 
         前処理の大きな流れ
@@ -50,10 +49,11 @@ def main(data_dir, save_dir):
     '''
     
     data_dir = Path(data_dir)
-    save_dir = Path(save_dir)
+    # save_dir = Path(save_dir)
     assert data_dir.name == 'train', f'data_dirのディレクトリ名の最後が"train"である必要があります。input: {str(data_dir)}'
 
     # 1. データを読み込む
+    print('1. データ読み込み')
     rain = pd.read_csv(data_dir / 'rainfall' / 'data.csv')
     rain_st = pd.read_csv(data_dir / 'rainfall' / 'stations.csv')
     tide = pd.read_csv(data_dir / 'tidelevel' / 'data.csv')
@@ -61,28 +61,37 @@ def main(data_dir, save_dir):
     water = pd.read_csv(data_dir / 'waterlevel' / 'data.csv')
     water_st = pd.read_csv(data_dir / 'waterlevel' / 'stations.csv')
     dam = pd.read_csv(data_dir / 'dam.csv')
+    print(' => complete')
 
     # 2. 3つのデータに対し前処理 & database化
-    rain = rain_process1(rain, rain_st)
-    tide = tide_process1(tide, tide_st)
-    water = water_process1(water, water_st)
+    print('2. 3つのデータに前処理 & database化')
+    rain, rain_st = rain_process1(rain, rain_st)
+    tide, tide_st = tide_process1(tide, tide_st)
+    water, water_st = water_process1(water, water_st)
+    print(' => complete')
 
     # 3. 1つのstationに対し値を一列に時系列で並べる
+    print('3. 1つのstationに対し値を一列に時系列で並べる')
     rain = convert_timeseries(rain)
     tide = convert_timeseries(tide)
     water = convert_timeseries(water)
+    print(' => complete')
 
     # 4. 河川名、水系名について処理
+    print('4. 河川名、水系名について処理')
     (rain_st, tide_st, water_st, dam, river_table, river_system_table) = \
         river_process1(rain_st, tide_st, water_st, dam)
+    print(' => complete')
 
     # 5. 保存
-    rain.to_csv(save_dir / 'rainfall' / 'data.csv', index=False)
-    rain_st.to_csv(save_dir / 'rainfall' / 'stations.csv', index=False)
-    tide.to_csv(save_dir / 'tidelevel' / 'data.csv', index=False)
-    tide_st.to_csv(save_dir / 'tidelevel' / 'stations.csv', index=False)
-    water.to_csv(save_dir / 'waterlevel' / 'data.csv', index=False)
-    water_st.to_csv(save_dir / 'waterlevel' / 'stations.csv', index=False)
-    dam.to_csv(save_dir / 'dam.csv', index=False)
-    river_table.to_csv(save_dir / 'river_table.csv', index=False)
-    river_system_table.to_csv(save_dir / 'river_system_table.csv', index=False)
+    # rain.to_csv(save_dir / 'rainfall' / 'data.csv', index=False)
+    # rain_st.to_csv(save_dir / 'rainfall' / 'stations.csv', index=False)
+    # tide.to_csv(save_dir / 'tidelevel' / 'data.csv', index=False)
+    # tide_st.to_csv(save_dir / 'tidelevel' / 'stations.csv', index=False)
+    # water.to_csv(save_dir / 'waterlevel' / 'data.csv', index=False)
+    # water_st.to_csv(save_dir / 'waterlevel' / 'stations.csv', index=False)
+    # dam.to_csv(save_dir / 'dam.csv', index=False)
+    # river_table.to_csv(save_dir / 'river_table.csv', index=False)
+    # river_system_table.to_csv(save_dir / 'river_system_table.csv', index=False)
+
+    return (rain, rain_st, tide, tide_st, water, water_st, dam, river_table, river_system_table)
