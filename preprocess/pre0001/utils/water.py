8adb18ce922666513b0a2464df26d2ebcafe0a95
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-12-18 12:25:25 +0900

    pre0001 fix

--- preprocess/pre0001/utils/water.py
+++ preprocess/pre0001/utils/water.py
@@ -4,9 +4,9 @@ def water_process1(water, water_st):
     # data.csvで異常値を取るものは、異常値をとらなくなってからの値を使う。
     # 今回は「異常値」 = 「前後で100近くの変化があった箇所」としている。
     # (以下三つ以外ではめちゃくちゃ大きくても10前後)
-    water = water[(water['date'] < 1304) & (water['station'] != '大谷池')]
-    water = water[(water['date'] < 699) & (water['station'] != "白川")]
-    water = water[(water['date'] < 94) & (water['station'] != "山手左岸(国)")]
+    water = water[~((water['date'] <= 1304) & (water['station'] == '大谷池'))]
+    water = water[~((water['date'] <= 699) & (water['station'] == "白川"))]
+    water = water[~((water['date'] <= 94) & (water['station'] == "山手左岸(国)"))]
 
     # data.csvのstation名についている(電)は全て消して良い
     water['station'] = water['station'].str.replace(r'\(電\)', '')
