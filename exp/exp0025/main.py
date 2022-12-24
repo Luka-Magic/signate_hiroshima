fef1955ea9d91ca7654742a078f2c87b0d2627a6
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2022-12-24 17:47:24 +0900

    exp0025 debug

--- exp/exp0025/main.py
+++ exp/exp0025/main.py
@@ -222,7 +222,7 @@ def train_one_epoch(cfg, epoch, dataloader, model, loss_fn, device, optimizer, s
     for step, (data, target, meta) in pbar:
         data = data.to(device).float() # (bs, len_of_series, input_size)
         target = target.to(device).float() # (bs, len_of_series)
-        stations = torch.tensor(list(map(int, meta['station']))).unsqueeze(-1).to(device).long(ß)
+        stations = torch.tensor(list(map(int, meta['station']))).unsqueeze(-1).to(device).long()
         print(stations.shape)
         print(stations)
         pred = model(data, target, stations, teacher_forcing_ratio).squeeze()
